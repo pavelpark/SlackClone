7c67e4bb7883a8d0acd0c4be966dec4d823e25d0
Pavel Parkhomey <pavelpark@aol.com>
2017-10-11 23:09:29 -0700

    Segue all setup for LoginView.

---
+++ Slack/Slack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Slack
+//
+//  Created by Pavel Parkhomey on 10/11/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
